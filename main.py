4fcbbfaa6d64cb6abf09c803395f83343bbfc59f
Gustavo Cervon <cervon.gus@gmail.com>
2024-09-28 20:08:29 -0600

    rotate triangle to left or right

--- main.py
+++ main.py
@@ -20,13 +20,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        player.update(dt)
+
         screen.fill(color=background_color)
         player.draw(screen)
         pygame.display.flip() # Refresh screen
-
-        dt = clock.tick(60) / 1000
         
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
