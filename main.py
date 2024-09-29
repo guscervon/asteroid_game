124d7b766ee9efc2d510bfb240dda67e5f9046d0
Gustavo Cervon <cervon.gus@gmail.com>
2024-09-28 19:46:49 -0600

    create triangle

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from constants import *
+from player import Player
 import pygame
 
 def main():
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
     pygame.init()
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    
+    player = Player(x, y)
+
     dt = 0
     background_color = (0,0,0) # RGB sequence for black
 
     while True:
+
         for event in pygame.event.get():
-            if event == pygame.QUIT:
+            if event.type == pygame.QUIT:
                 return
             
         screen.fill(color=background_color)
+        player.draw(screen)
         pygame.display.flip() # Refresh screen
 
         dt = clock.tick(60) / 1000
