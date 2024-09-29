5a2e0826ef5a5853676587565d46fac391f33892
Gustavo Cervon <cervon.gus@gmail.com>
2024-09-28 19:04:05 -0600

    set fps to 60

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ import pygame
 def main():
 
     pygame.init()
-
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    dt = 0
     background_color = (0,0,0) # RGB sequence for black
 
     while True:
         for event in pygame.event.get():
             if event == pygame.QUIT:
                 return
-            screen.fill(color=background_color)
-            pygame.display.flip() # Refresh screen
+            
+        screen.fill(color=background_color)
+        pygame.display.flip() # Refresh screen
+
+        dt = clock.tick(60) / 1000
         
 
 
